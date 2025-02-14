4c72c5d0a3137bfb7564ab0dba38556411dcb78a
Feng Hualong <hualong.feng@intel.com>
2025-02-14 10:11:20 +0800

    Update csi_plugin.py
    IdentityService Plugin Info name 应该和
    CSIDriver中的metadata的name保持一致。
    
    Signed-off-by: Feng Hualong <hualong.feng@intel.com>

--- csi_plugin.py
+++ csi_plugin.py
@@ -1,21 +1,28 @@
-from concurrent import futures  
-import grpc  
-import logging  
+from concurrent import futures
+import grpc
+import logging
 import os
-from csi_pb2 import (  
-    GetPluginInfoResponse,  
-    GetPluginCapabilitiesResponse,  
-    PluginCapability,  
-    ProbeResponse,  
-    CreateVolumeResponse,  
-    Volume,  
-    DeleteVolumeResponse,  
+from csi_pb2 import (
+    GetPluginInfoResponse,
+    GetPluginCapabilitiesResponse,
+    PluginCapability,
+    ProbeResponse,
+    CreateVolumeResponse,
+    Volume,
+    DeleteVolumeResponse,
     NodeStageVolumeResponse,
     NodeUnstageVolumeResponse,
     ControllerPublishVolumeResponse,
     ControllerUnpublishVolumeResponse,
-)  
-from csi_pb2_grpc import IdentityServicer, ControllerServicer, NodeServicer, add_IdentityServicer_to_server, add_ControllerServicer_to_server, add_NodeServicer_to_server  
+)
+from csi_pb2_grpc import (
+    IdentityServicer,
+    ControllerServicer,
+    NodeServicer,
+    add_IdentityServicer_to_server,
+    add_ControllerServicer_to_server,
+    add_NodeServicer_to_server,
+)
 
 # Configure logging to include time, filename, and log message
 logging.basicConfig(
@@ -24,37 +31,36 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-class IdentityService(IdentityServicer):  
-    def GetPluginInfo(self, request, context):  
+class IdentityService(IdentityServicer):
+    def GetPluginInfo(self, request, context):
         logging.info("GetPluginInfo called")
-        return GetPluginInfoResponse(name="my-csi-plugin", vendor_version="v0.1")  
+        return GetPluginInfoResponse(name="hostpath.csi.k8s.io", vendor_version="v0.1")
 
-    def GetPluginCapabilities(self, request, context):  
+    def GetPluginCapabilities(self, request, context):
         logging.info("GetPluginCapabilities called")
-        return GetPluginCapabilitiesResponse(  
-            capabilities=[PluginCapability(service=PluginCapability.Service(type=PluginCapability.Service.CONTROLLER_SERVICE))]  
-        )  
+        return GetPluginCapabilitiesResponse(
+            capabilities=[PluginCapability(service=PluginCapability.Service(type=PluginCapability.Service.CONTROLLER_SERVICE))]
+        )
 
-    def Probe(self, request, context):  
+    def Probe(self, request, context):
         logging.info("Probe called")
-        # Implement health check logic here
         return ProbeResponse(ready=True)
 
-class ControllerService(ControllerServicer):  
-    def CreateVolume(self, request, context):  
+class ControllerService(ControllerServicer):
+    def CreateVolume(self, request, context):
         logging.info("CreateVolume called")
         volume_id = request.name
         volume_path = f"/mnt/hostpath/{volume_id}"
         os.makedirs(volume_path, exist_ok=True)
         return CreateVolumeResponse(volume=Volume(volume_id=volume_id, capacity_bytes=request.capacity_range.required_bytes))
 
-    def DeleteVolume(self, request, context):  
+    def DeleteVolume(self, request, context):
         logging.info("DeleteVolume called")
         volume_id = request.volume_id
         volume_path = f"/mnt/hostpath/{volume_id}"
         if os.path.exists(volume_path):
             os.rmdir(volume_path)
-        return DeleteVolumeResponse()  
+        return DeleteVolumeResponse()
 
     def ControllerPublishVolume(self, request, context):
         logging.info("ControllerPublishVolume called")
@@ -64,8 +70,8 @@ class ControllerService(ControllerServicer):
         logging.info("ControllerUnpublishVolume called")
         return ControllerUnpublishVolumeResponse()
 
-class NodeService(NodeServicer):  
-    def NodeStageVolume(self, request, context):  
+class NodeService(NodeServicer):
+    def NodeStageVolume(self, request, context):
         logging.info("NodeStageVolume called")
         staging_target_path = request.staging_target_path
         volume_id = request.volume_id
@@ -73,22 +79,22 @@ class NodeService(NodeServicer):
         os.symlink(volume_path, staging_target_path)
         return NodeStageVolumeResponse()
 
-    def NodeUnstageVolume(self, request, context):  
+    def NodeUnstageVolume(self, request, context):
         logging.info("NodeUnstageVolume called")
         staging_target_path = request.staging_target_path
-        if os.path.islink(staging_target_path):
+        if os.islink(staging_target_path):
             os.unlink(staging_target_path)
         return NodeUnstageVolumeResponse()
 
-def serve():  
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))  
-    add_IdentityServicer_to_server(IdentityService(), server)  
-    add_ControllerServicer_to_server(ControllerService(), server)  
-    add_NodeServicer_to_server(NodeService(), server)  
-    server.add_insecure_port("[::]:50051")  
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_IdentityServicer_to_server(IdentityService(), server)
+    add_ControllerServicer_to_server(ControllerService(), server)
+    add_NodeServicer_to_server(NodeService(), server)
+    server.add_insecure_port("[::]:50051")
     logging.info("Starting CSI plugin...")
-    server.start()  
-    server.wait_for_termination()  
+    server.start()
+    server.wait_for_termination()
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     serve()
